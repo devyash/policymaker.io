73197fb63536e8f21623626bd0c86df8803da511
Devyash <devyashsanghai@gmail.com>
2017-04-05 22:23:18 -0400

    Added employment, and other routes

---
+++ server/routes/emploment.js
@@ -0,0 +1,43 @@
+/*jslint node: true, indent: 2 */
+'use strict';
+let Promise,oracledb,SimpleOracleDB;
+Promise = require("bluebird"); //favourite promise library
+oracledb = require('oracledb');
+SimpleOracleDB = require('simple-oracledb');
+SimpleOracleDB.extend(oracledb);
+oracledb.Promise = Promise;//setting promise library tobluebird
+oracledb.outFormat = oracledb.OBJECT;
+// Get Oracle Connection only 1 connection 
+var connection = oracledb.getConnection(
+  {
+    // user          : "hv0",
+    // password      : "Spide123!",
+    user          : "harika",
+    password      : "Oracleme893!",
+    connectString : "oracle.cise.ufl.edu:1521/orcl",
+  });
+
+function respond(req, res, next) {
+  if (req.params.year) {
+    console.log(req.params.year);
+  }
+    connection.then(function(conn) {
+      return conn.execute("SELECT * FROM employment")
+        .then(function(result) {
+          let output=result.rows;
+          res.send(output);
+          //console.log(result.rows);
+          //return conn.close();
+        })
+
+    })
+    .catch(function(err) {
+      console.error(err);
+    });
+
+ }
+
+module.exports = function (server) {
+server.get('/employment', respond);
+
+};
