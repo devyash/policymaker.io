00c01f30fc5c67f3f589348bca4580341d246212
Devyash <devyashsanghai@gmail.com>
2017-04-15 05:02:29 -0400

    Loading dynamic graphs

--- client web app/src/components/ActivityGraph.vue
+++ client web app/src/components/ActivityGraph.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="ActivityGraph">
     <div>
-<bar-chart>Yo</bar-chart>
+<bar-chart :labels=labels :row=row>Yo</bar-chart>
   </div>
   </div>
 </template>
@@ -9,6 +9,12 @@
 <script>
 import BarChart from './BarChart'
 export default {
+  data: function(){
+    return {
+      labels: ['LOLOL', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+       row: [40, 20, 12, 39, 10, 40, 39, 80, 40, 20, 12, 11]
+    }
+  },
   components:{
     'bar-chart':BarChart
   },
--- client web app/src/components/BarChart.js
+++ client web app/src/components/BarChart.js
@@ -2,16 +2,23 @@
 import { Bar } from 'vue-chartjs'
 
 export default Bar.extend({
-  mounted () {
+  props: ['labels','row'],
+  data: function (){
+    return {
+      locallabels:this.labels,
+      localrow:this.row
+      }
+    },
+    mounted () {
     // Overwriting base render method with actual data.
     this.renderChart({
-      labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+      labels: this.locallabels,
       datasets: [
-        {
-          label: 'GitHub Commits',
-          backgroundColor: '#f87979',
-          data: [40, 20, 12, 39, 10, 40, 39, 80, 40, 20, 12, 11]
-        }
+      {
+        label: 'GitHub Commits',
+        backgroundColor: '#f87979',
+        data: this.localrow
+      }
       ]
     }, {responsive:true, maintainAspectRatio: false })
   }
