61161f153eea83a3baa783ab06e019caaa13e61a
Harika <harikasharma93@gmail.com>
2017-04-15 19:15:05 -0400

    Added employment query1 endpoint

--- server/routes/emploment.js
+++ server/routes/emploment.js
@@ -10,23 +10,31 @@ oracledb.outFormat = oracledb.OBJECT;
 // Get Oracle Connection only 1 connection 
 var connection = oracledb.getConnection(
   {
-    // user          : "hv0",
-    // password      : "Spide123!",
-    user          : "harika",
-    password      : "Oracleme893!",
+    user          : "hv0",
+    password      : "Spide123!",
+    //user          : "harika",
+    //password      : "Oracleme893!",
     connectString : "oracle.cise.ufl.edu:1521/orcl",
   });
 
 function respond(req, res, next) {
 
-  if (req.params.type === 'simple') {
-    
-    if (req.params.selectrange === 'top')  {
-    
+      
       connection.then(function(conn) {
-      return conn.execute("select * from (select state,sum(" + req.params.criteria + ") as total from employment group by state order by total desc) where ROWNUM<="+ req.params.N+";")
+
+        let N1=parseInt(req.params.N);
+        let top_bot=req.params.criteria;
+        let attribute1=req.params.selectrange;
+
+        let query="select * from TABLE(emp_pkg1.emp_topbot_fun(:top,:employed,:id))" 
+      return conn.execute(query,{
+        top: top_bot,
+        employed: attribute1,
+        id: N1
+      })
         .then(function(result) {
           let output=result.rows;
+          console.log(output);
           res.send(output);
         })
 
@@ -36,26 +44,6 @@ function respond(req, res, next) {
     });
 
     }
-
-    else {
-
-      connection.then(function(conn) {
-      return conn.execute("select * from (select state,sum(" + req.params.criteria + ") as total from employment group by state order by total) where ROWNUM<="+ req.params.N+";")
-        .then(function(result) {
-          let output=result.rows;
-          res.send(output);
-        })
-
-    })
-    .catch(function(err) {
-      console.error(err);
-    });
-
-    }
-    
-  }
-
- }
 
 module.exports = function (server) {
 server.get('/employment', respond);
